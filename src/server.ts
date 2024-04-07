15e3937321e8fd1d3e72ebc4eb19ec12f57d73b4
Marcos Gomes <marcosgomesdev@gmail.com>
2024-04-07 00:31:55 -0300

    feat: add configurations and implement routers

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,33 @@
+import fastifyCors from "@fastify/cors";
+import "dotenv/config";
 import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { createTranscription } from "./routes/create-transcription";
+import { generateAICompletion } from "./routes/generate-ai-completion";
+import { getAllPrompts } from "./routes/get-all-prompts";
+import { uploadVideo } from "./routes/upload-video";
 
 const app = fastify();
 
-app.get("/", async (request, reply) => {
-  return { hello: "world" };
+app.register(fastifyCors, {
+  origin: "*",
 });
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(getAllPrompts);
+app.register(uploadVideo);
+app.register(createTranscription);
+app.register(generateAICompletion);
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log("HTTP Server Running!");
+    console.log("HTTP Server Running! 🚀");
   });
