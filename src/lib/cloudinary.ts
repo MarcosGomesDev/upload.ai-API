65e81cdd23a8a5c2c5d617e0b6989da6c696cc4f
Marcos Gomes <marcosgomesdev@gmail.com>
2024-04-07 00:42:00 -0300

    feat: add cloudnary

---
+++ src/lib/cloudinary.ts
@@ -0,0 +1,7 @@
+import * as Cloudinary from "cloudinary";
+
+export const cloudinary = Cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
